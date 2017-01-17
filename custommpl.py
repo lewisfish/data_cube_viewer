680297f7c0c7a18d3c9bd5e0514380cfa746bab4
lewis mcmillan <lewistmcmillan@gmail.com>
2017-01-17 17:05:31 +0000

    added ability to change 4th dimension when viewing data cubes. added autoscale shortcut Ctrl-a

--- custommpl.py
+++ custommpl.py
@@ -10,16 +10,20 @@ from matplotlib.backends.backend_qt4agg import (
 Ui_MainWindow, QMainWindow = loadUiType('/home/lewis/data_cube/mainwindow.ui')
 
 
-## weird bug where if you change view plane to y or z then open a new file it plots in two subplots. seems to work if you select x view again?!?!?!
+# weird bug where if you change view plane to y or z then open a new file it plots in two subplots. seems to work if you select x view again?!?!?!
 
 class Main(QMainWindow, Ui_MainWindow):
 
     def __init__(self, ):
         super(Main, self).__init__()
         self.setupUi(self)
+        self.showMaximized()
         self.flag = 0
         self.ave = np.array([])
         self.auto_flag = False
+        self.spinBoxval = 0
+        self.spinBox.hide()
+
 
         self.XView.setChecked(True)
         self.fig = Figure()
@@ -44,18 +48,27 @@ class Main(QMainWindow, Ui_MainWindow):
         self.Reset.triggered.connect(self.reset_plot)
         self.AutoScale.triggered.connect(self.Auto_Scale_plot)
         self.Bore_View.triggered.connect(self.ViewBore)
-        
+
+        self.spinBox.valueChanged.connect(self.changeSpinbox)
+
+    def changeSpinbox(self):
+        self.spinBoxval = int(self.spinBox.value())
+        fd = open(self.name, 'rb')
+        self.readslice(fd, 201, 201, 201, np.float64, 4)
+        self.reset_plot()
+        self.init_plot()
+
     def ViewBore(self):
         self.AveBoreView = self.showBvDialog()
         self.BoreChecked()
-        
+
     def Auto_Scale_plot(self):
-        print 'toggle',self.auto_flag
+        # print 'toggle', self.auto_flag
         if not self.auto_flag:
             self.auto_flag = True
         else:
             self.auto_flag = False
-    
+
     def sliderval(self):
         try:
             if self.XView.isChecked():
@@ -72,7 +85,7 @@ class Main(QMainWindow, Ui_MainWindow):
                 self.Scroll_Vert.setValue(self.ind)
         except IndexError:
             pass
-        
+
         try:
             self.im.axes.figure.canvas.draw()
             if self.auto_flag:
@@ -96,7 +109,7 @@ class Main(QMainWindow, Ui_MainWindow):
         # self.im.autoscale()
 
     def saveBore(self,):
-    
+
         name = QtGui.QFileDialog.getSaveFileName(self, 'Save File')
         f = open(name, 'w')
         if len(self.ave) > 1:
@@ -106,6 +119,7 @@ class Main(QMainWindow, Ui_MainWindow):
         else:
             print('Error saving File!')
             print('No Data!!')
+
     def file_open(self, *args):
         if self.flag == 1:
             self.reset_plot()
@@ -115,13 +129,16 @@ class Main(QMainWindow, Ui_MainWindow):
             pass
         if args[0]:
             args = args[0]
-            name = args[0]
+            self.name = args[0]
             if len(args) == 2:
-                ndim = args[1]
+                # ndim = args[1]
+                ndim = (args[1], args[1], args[1])
+
                 item = str(self.showDtDialog())
 
             elif len(args) == 3:
-                ndim = args[1]
+                # ndim = args[1]
+                ndim = (args[1], args[1], args[1])
                 item = args[2]
 
                 if int(item) == 1:
@@ -137,7 +154,7 @@ class Main(QMainWindow, Ui_MainWindow):
                 item = str(self.showDtDialog())
             args = None
         else:
-            name = QtGui.QFileDialog.getOpenFileName(self, 'Open File')
+            self.name = QtGui.QFileDialog.getOpenFileName(self, 'Open File')
             ndim = self.showNdimDialog()
             item = str(self.showDtDialog())
 
@@ -148,16 +165,17 @@ class Main(QMainWindow, Ui_MainWindow):
 
         if "4 dim" in item:
             dim = 4
+            self.spinBox.show()
         elif "3 dim" in item:
             dim = 3
 
         try:
-            fd = open(name, 'rb')
+            fd = open(self.name, 'rb')
         except IOError:
             pass
         try:
-            #del self.X
-            self.readslice(fd, int(ndim), dt, dim)
+            # del self.X
+            self.readslice(fd, int(ndim[0]), int(ndim[1]), int(ndim[2]), dt, dim)
             self.init_plot()
         except ValueError:
             self.ErrorDialog("Value of Ndim incorrect for this data cube.")
@@ -170,7 +188,7 @@ class Main(QMainWindow, Ui_MainWindow):
                 self.reset_plot()
                 self.im = self.ax1.matshow(self.X[self.ind, :, :],
                                            cmap='cubehelix', interpolation='nearest')
-                self.fig.colorbar(self.im) 
+                self.fig.colorbar(self.im)
                 self.addmpl()
 
         if b.text() == "Y View":
@@ -198,27 +216,27 @@ class Main(QMainWindow, Ui_MainWindow):
         if b.text() == "Avg. Bore":
             if b.isChecked() is True:
                 self.BoreChecked()
-    
+
     def BoreChecked(self):
         if self.AveBoreView == 'X':
             self.view = (1, 2)
         elif self.AveBoreView == 'Y':
             self.view = (0, 2)
         elif self.AveBoreView == 'Z':
-           self.view = (0, 1)
+            self.view = (0, 1)
         self.reset_plot()
 
         if len(self.ave) == 0:
             self.ave = np.array([])
             self.ave = np.sum(self.X, self.view)
-            self.ave /= (len(self.X[0]) * len(self.X[1]))
+            self.ave /= (len(self.X[self.view[0]]) * len(self.X[self.view[1]]))
 
         self.im = self.ax1.plot(self.ave)
         self.addmpl()
 
     def reset_plot(self, ):
         self.ave = np.array([])
-        gc.collect()#fixes most of memory leak
+        gc.collect()  # fixes most of memory leak
         self.fig.clf()
         self.ax1.clear()
 
@@ -238,10 +256,10 @@ class Main(QMainWindow, Ui_MainWindow):
             self.rmmpl()
         except:
             pass
-        self.XView.setChecked(True)
+        # self.XView.setChecked(True)
         self.addmpl()
         rows, cols, self.slices = self.X.shape
-        self.ind = self.slices / 2
+        self.ind = rows / 2
         self.im = self.ax1.matshow(self.X[self.ind, :, :],
                                    cmap='cubehelix', interpolation='nearest')
         self.fig.colorbar(self.im)
@@ -250,29 +268,33 @@ class Main(QMainWindow, Ui_MainWindow):
         self.Scroll_Vert.setMaximum(self.slices)
         self.Scroll_Vert.setValue(self.ind)
 
-    def readslice(self, fd, ndim, dt, dim):
+    def readslice(self, fd, dimx, dimy, dimz, dt, dim):
         if dim == 4:
             if dt == np.float64:
                 magic = 4        # !this sometimes needs changed try powers of 2...
             elif dt == np.float32:
                 magic = 4
-            shape = (ndim, ndim, ndim, magic)
+            shape = (dimx, dimy, dimz, magic)
         elif dim == 3:
-            shape = (ndim, ndim, ndim)
+            shape = (dimx, dimy, dimz)
         data = np.fromfile(
             file=fd, dtype=dt, sep="")
-#        print len(data)
+        # print len(data)
         data = data.reshape(shape, order='F')
         fd.close()
         if dim == 4:
-            data = data[:, :, :, 0]
+            data = data[:, :, :, self.spinBoxval]
         self.X = data[:, :, :]
         del data
 
     def showNdimDialog(self, ):
-        text, ok = QtGui.QInputDialog.getInt(self, 'Input Ndim', 'Enter Ndim:')
-        if ok:
-            return text
+        text1, ok1 = QtGui.QInputDialog.getInt(self, 'Input Ndim', 'Enter Ndim:')
+        if ok1:
+            text2, ok2 = QtGui.QInputDialog.getInt(self, 'Input Ndim', 'Enter Ndim:')
+            if ok2:
+                text3, ok3 = QtGui.QInputDialog.getInt(self, 'Input Ndim', 'Enter Ndim:')
+                if ok3:
+                    return (text1, text2, text3)
 
     def showDtDialog(self, ):
         items = ("4 dim Real*4", "4 dim Real*8",
@@ -286,13 +308,11 @@ class Main(QMainWindow, Ui_MainWindow):
 
     def showBvDialog(self, ):
         items = ("X", "Y", "Z")
-        
         item, ok = QtGui.QInputDialog.getItem(self, "Select Average Bore Direction",
                                               "Views", items, 0, False)
-                                               
         if ok and item:
             return item
-        
+
     def ErrorDialog(self, ErrMsg):
         QtGui.QMessageBox.warning(self, "Error", ErrMsg)
 
@@ -300,16 +320,16 @@ if __name__ == '__main__':
     import sys
     from PyQt4 import QtGui
     import numpy as np
-    
-    if len(sys.argv) >=2:
-        if 'help' in str(sys.argv[1]) or str(sys.argv[1])=='-h':
+
+    if len(sys.argv) >= 2:
+        if 'help' in str(sys.argv[1]) or str(sys.argv[1]) == '-h':
             print '\nUsage: d3v [FILE] [NDIM] [FP_REP+DIM](1-4)'
 
             print'\nNDIM, \t    gives the dimensions of the data cube to be examined'
             print'FP_REP+DIM, gives choice of(1-4):\n\n\t4 dim Real*4, 4 dim Real*8, 3 dim Real*4, 3 dim Real*8'
             print'\t    (1)\t\t  (2)\t\t(3)\t      (4)\n'
             sys.exit(0)
-            
+
         elif len(sys.argv) < 3 and len(sys.argv) > 2:
             print '\nUsage: d3v [FILE] [NDIM] [FP_REP+DIM](1-4)'
 
